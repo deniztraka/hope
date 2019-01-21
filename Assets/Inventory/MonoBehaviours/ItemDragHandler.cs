d05ca97fc2d7e894752cca871707fd4baa05fae8
deniztraka <deniz.traka@gmail.com>
2019-01-22 00:04:47 +0300

    inv

---
+++ Assets/Inventory/MonoBehaviours/ItemDragHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("asdasd");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
