14339dbd9977de06f44e7e1232995dc7c6891d5b
deniztraka <deniz.traka@gmail.com>
2019-02-04 01:12:43 +0300

    Options menu

---
+++ Assets/Scripts/UI/Components/OptionsButtonBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsButtonBehaviour : MonoBehaviour
+{
+
+
+    public void OnClick()
+    {
+        var results = Resources.FindObjectsOfTypeAll<ExitGameCanvasPopUp>();
+        var exitGameCanvas = results[0].gameObject;
+        exitGameCanvas.SetActive(true);
+    }
+}
