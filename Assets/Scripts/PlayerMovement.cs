a629cc81aa12de48fd6c26ae9ee0441ff81f5d57
deniztraka <deniz.traka@gmail.com>
2019-02-04 00:51:22 +0300

    Some UI updates.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,32 +17,36 @@ public class PlayerMovement : MonoBehaviour
         runSpeed = RunSpeed;
     }
 
+    public void SetHorizontalMovement(float direction){
+        horizontalMove = direction * runSpeed;
+    }
+
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.touchCount > 0)
-        {
-            var touch = Input.GetTouch(0);
+        // if (Input.touchCount > 0)
+        // {
+        //     var touch = Input.GetTouch(0);
 
-			var direction = 0f;
-			if (touch.position.x < Screen.width / 8f)
-            {
+		// 	var direction = 0f;
+		// 	if (touch.position.x < Screen.width / 8f)
+        //     {
 
-                direction = -1f;
-            }
-            else if (touch.position.x > (Screen.width * (7f / 8f)))
-            {
-				direction = 1f;
-            }
+        //         direction = -1f;
+        //     }
+        //     else if (touch.position.x > (Screen.width * (7f / 8f)))
+        //     {
+		// 		direction = 1f;
+        //     }
 
-			horizontalMove = direction * runSpeed;
+		// 	horizontalMove = direction * runSpeed;
             
-        }
-        else
-        {
-            horizontalMove = 0f;
-        }
+        // }
+        // else
+        // {
+        //     horizontalMove = 0f;
+        // }
 
 
         if (Input.GetButtonDown("Jump"))
