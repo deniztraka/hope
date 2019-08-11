3ba3df9adce754350ee0af88b3694c590563c808
deniztraka <deniz.traka@gmail.com>
2019-08-12 02:52:29 +0300

    Isometric Player Anim

--- Assets/Scripts/CharacterAnimatorHandler.cs
+++ Assets/Scripts/CharacterAnimatorHandler.cs
@@ -6,7 +6,8 @@ public class CharacterAnimatorHandler : MonoBehaviour
 {
     public static readonly string[] staticDirections = { "Static N", "Static NW", "Static W", "Static SW", "Static S", "Static SE", "Static E", "Static NE" };
     public static readonly string[] staticDirectionsSimple = { "idle_north", "idle_west", "idle_south", "idle_east" };
-    public static readonly string[] walkingDirectionsSimple = { "walking_north", "walking_west", "walking_south", "walking_east" };
+    
+    public static readonly string[] walkingDirections = {"walking_north", "walking_north_west", "walking_west", "walking_south_west", "walking_south", "walking_south_east", "walking_east", "walking_north_east"};
 
     Animator animator;
     int lastDirection;
@@ -17,8 +18,9 @@ public class CharacterAnimatorHandler : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    public void SetDirection(Vector2 direction)
-    {
+
+    public void SetDirection(Vector2 direction){
+
         //use the Run states by default
         string[] directionArray = null;
 
@@ -27,30 +29,30 @@ public class CharacterAnimatorHandler : MonoBehaviour
         {
             //if we are basically standing still, we'll use the Static states
             //we won't be able to calculate a direction if the user isn't pressing one, anyway!
-            directionArray = staticDirectionsSimple;
+            directionArray = staticDirections;
         }
         else
         {
             //we can calculate which direction we are going in
             //use DirectionToIndex to get the index of the slice from the direction vector
             //save the answer to lastDirection
-            directionArray = walkingDirectionsSimple;
-            lastDirection = DirectionToIndex(direction, 4);
+            directionArray = walkingDirections;
+            lastDirection = DirectionToIndex(direction, 8);
         }
-        
+
         //tell the animator to play the requested state
         animator.Play(directionArray[lastDirection]);
     }
 
+    //helper functions
+
     //this function converts a Vector2 direction to an index to a slice around a circle
     //this goes in a counter-clockwise direction.
-    public static int DirectionToIndex(Vector2 dir, int sliceCount)
-    {
+    public static int DirectionToIndex(Vector2 dir, int sliceCount){
         //get the normalized direction
         Vector2 normDir = dir.normalized;
         //calculate how many degrees one slice is
-        float step = 360f / sliceCount;       
-
+        float step = 360f / sliceCount;
         //calculate how many degress half a slice is.
         //we need this to offset the pie, so that the North (UP) slice is aligned in the center
         float halfstep = step / 2;
@@ -60,8 +62,7 @@ public class CharacterAnimatorHandler : MonoBehaviour
         //add the halfslice offset
         angle += halfstep;
         //if angle is negative, then let's make it positive by adding 360 to wrap it around.
-        if (angle < 0)
-        {
+        if (angle < 0){
             angle += 360;
         }
         //calculate the amount of steps required to reach this angle
@@ -69,6 +70,12 @@ public class CharacterAnimatorHandler : MonoBehaviour
         //round it, and we have the answer!
         return Mathf.FloorToInt(stepCount);
     }
+
+
+
+
+
+
 
     //this function converts a string array to a int (animator hash) array.
     public static int[] AnimatorStringArrayToHashArray(string[] animationArray)
