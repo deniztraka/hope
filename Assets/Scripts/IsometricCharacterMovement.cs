9d144e7e6a14ed0e93e4de1e72ff07538e13b2d4
deniztraka <deniz.traka@gmail.com>
2019-09-02 18:03:07 +0300

    joystick is added for movement

--- Assets/Scripts/IsometricCharacterMovement.cs
+++ Assets/Scripts/IsometricCharacterMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class IsometricCharacterMovement : MonoBehaviour
 {
+    public FixedJoystick joystick;
+
     public bool isRunning = false;
     public float movementSpeed = 1f;
     public CharacterAnimatorHandler characterAnimatorHandler;
@@ -20,13 +22,20 @@ public class IsometricCharacterMovement : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 currentPos = rbody.position;
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");        
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");        
+        float horizontalInput = joystick.Horizontal;
+        float verticalInput = joystick.Vertical;
+
         Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
         //inputVector = Vector2.ClampMagnitude(inputVector, 1);
         Vector2 movement = inputVector * movementSpeed * (isRunning ? 1.5f : 1f);
         Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
         characterAnimatorHandler.SetDirection(movement, isRunning);
         rbody.MovePosition(newPos);
+
+        //Vector3 direction = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
+        //rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
     }
 }
